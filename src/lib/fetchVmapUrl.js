bd5eb9eb8fa17787c598c5a0e407bb8f434581f1
Craig Wermert <craig.wermert@frontrow.cc>
2024-04-30 14:57:52 -0400

    parse xml

--- src/lib/fetchVmapUrl.js
+++ src/lib/fetchVmapUrl.js
@@ -7,8 +7,16 @@ export const fetchVmapUrl = (url) => new Promise((resolve, reject) => {
   xhr.onreadystatechange = () => {
     if (xhr.readyState === xhr.DONE) {
       if (xhr.status === 200) {
+        let xmlDoc;
+        if (!xhr.responseXML) {
+          const parser = new DOMParser();
+          xmlDoc = parser.parseFromString(xhr.responseText, 'text/xml');
+        } else {
+          xmlDoc = xhr.responseXML;
+        }
+
         // Get a parsed VMAP object
-        const vmap = new VMAP(xhr.responseXML);
+        const vmap = new VMAP(xmlDoc);
         resolve(vmap);
       } else {
         reject(new Error('Error'));
